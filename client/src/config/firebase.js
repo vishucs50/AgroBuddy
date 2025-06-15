f46ea283cb7f5b48b2bda73dce3f5d3215d71e3b
vishu <vishujain.activa@gmail.com>
2025-06-15 14:59:32 +0530

    changes

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,10 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD6urCyyIZfrIVU3yK-SHkWKvnIhA__1Oc",
-  authDomain: "agrobuddy-4d0f8.firebaseapp.com",
-  projectId: "agrobuddy-4d0f8",
-  // Optional: storageBucket, messagingSenderId, etc. cloud storage for auth
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 };
- const app=initializeApp(firebaseConfig);
- export const auth=getAuth(app);
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
